20462d55579d4121c71406849b58351d61dc6072
s4mkofftrid <vadym.samkov@itrident.agency>
2026-03-08 21:28:26 +0200

    Fixed desktop size

--- composeApp/src/jvmMain/kotlin/s4/tools/wallpaper_changer/main.kt
+++ composeApp/src/jvmMain/kotlin/s4/tools/wallpaper_changer/main.kt
@@ -1,7 +1,13 @@
 package s4.tools.wallpaper_changer
 
+import androidx.compose.ui.platform.LocalViewConfiguration
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import kotlinx.coroutines.runBlocking
 import s4.tools.wallpaper_changer.data.MainFun
 import s4.tools.wallpaper_changer.data.api.wallhaven.WallhavenApi
@@ -24,8 +30,13 @@ fun main(
     } else {
         application {
             Window(
+                state = rememberWindowState().also {
+                    it.size = DpSize(360.dp, 640.dp)
+                    it.placement = WindowPlacement.Floating
+                },
                 onCloseRequest = ::exitApplication,
-                title = "wallpaper_changer",
+                resizable = false,
+                title = "Wallpaper Changer",
             ) {
                 App()
             }
